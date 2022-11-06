fa834acbd18d14aa7839f88c92c9d8e92f866cf1
optikrus <optikrus61@gmail.com>
2022-11-06 18:57:54 +0300

    edit version

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.exceptions import HTTPException
 from parser_class import CianParser
 from estimate import PoolEstimate
 
-app = FastAPI(title="Real estate calculation", version="0.1.1")
+app = FastAPI(title="Real estate calculation", version="0.1.2")
 
 origins = [
     "http://localhost:3000",
